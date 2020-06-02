d2c74a8a14130c8e28d035ee44c7a67fc67a7082
Sheldon Roberts <robeshel@flip2.engr.oregonstate.edu>
2020-06-02 12:11:58 -0700

    commit message

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
 
     function getUser(res, mysql, context, complete){
-        mysql.pool.query("SELECT user_id as id, username, password FROM users", function(error, results, fields){
+        mysql.pool.query("SELECT user_id as id, username, password FROM users WHERE username=req.body.username AND password=req.body.password", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -12,22 +12,6 @@ module.exports = function(){
             complete();
         });
     }
-
-
-router.get('/', function(req, res){
-        var callbackCount = 0;
-        var context = {};
-        var mysql = req.app.get('mysql');
-        getMedia(res, mysql, context, complete);
-        function complete(){
-            callbackCount++;
-            if(callbackCount >= 1){
-                res.render('login', context);
-            }
-
-        }
-    });
-
 
 router.post('/', function(req, res){
         console.log(req.body)
